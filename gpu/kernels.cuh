5a15a87a9cdeebda6d88325db03288b1d90c0e7b
babsubra1980 <37005639+babsubra1980@users.noreply.github.com>
2023-11-06 16:05:03 -0600

    Update kernels.cuh

--- gpu/kernels.cuh
+++ gpu/kernels.cuh
@@ -28,8 +28,8 @@ using std::endl;
 //Kernels for Version 2 - Kernel with Grid Stride for Bellman Ford relax operation
 //__global__ void relaxWithGridStride(int N, int MAX_VAL, int *d_in_V, int *d_in_I, int *d_in_E, int *d_in_W, int *d_out_D, int *d_out_Di);
 //__global__ void updateDistanceWithGridStride(int N, int *d_in_V, int *d_in_I, int *d_in_E, int *d_in_W, int *d_out_D, int *d_out_Di);
-//__global__ void updateIndexOfEdgesWithGridStide(int N, int *d_in_V, int *d_in_E, int l, int r);
-//__global__ void initializeArrayWithGridStride(const int N, int *p, const int val, bool sourceDifferent, const int source, const int sourceVal);
+__global__ void updateIndexOfEdgesWithGridStide(int N, int *d_in_V, int *d_in_E, int l, int r);
+__global__ void initializeArrayWithGridStride(const int N, int *p, const int val, bool sourceDifferent, const int source, const int sourceVal);
 
 //Kernels for Version 3 - Kernel with Grid Stride and boolean array to perform relax operation only if the source vertex has a lesser distance than the previous iteration
 __global__ void initializeBooleanArrayWithGridStride(const int N, bool *p, const int val, bool sourceDifferent, const int source, const bool sourceVal);
