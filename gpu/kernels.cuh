6882ead4d1e44a29a16476d141347d42feed76c2
babsubra1980 <37005639+babsubra1980@users.noreply.github.com>
2023-11-06 17:17:49 -0600

    Update kernels.cuh

--- gpu/kernels.cuh
+++ gpu/kernels.cuh
@@ -1,5 +1,5 @@
-#ifndef GPU_GRAPH_ALGORITHMS_KERNELS_CUH
-#define GPU_GRAPH_ALGORITHMS_KERNELS_CUH
+#ifndef BELLMANFORD_GPU_KERNELS_CUH
+#define BELLMANFORD_GPU_KERNELS_CUH
 #include <iostream>
 #include <cuda.h>
 #include <cuda_runtime.h>
@@ -18,7 +18,6 @@
 using std::cout;
 using std::endl;
 
-//Kernels for Grid Stride and boolean array to perform relax operation only if the source vertex has a lesser distance than the previous iteration
 __global__ void updateIndexOfEdgesWithGridStide(int N, int *d_in_V, int *d_in_E, int l, int r);
 __global__ void initializeArrayWithGridStride(const int N, int *p, const int val, bool sourceDifferent, const int source, const int sourceVal);
 __global__ void initializeBooleanArrayWithGridStride(const int N, bool *p, const int val, bool sourceDifferent, const int source, const bool sourceVal);
@@ -26,4 +25,4 @@ __global__ void relaxWithGridStrideV3(int N, int MAX_VAL, int *d_in_V, int *d_in
 __global__ void updateDistanceWithGridStrideV3(int N, int *d_in_V, int *d_in_I, int *d_in_E, int *d_in_W, int *d_out_D, int *d_out_Di, bool *p_Flag);
 __global__ void updatePredWithGridStride(int N, int *d_in_V, int *d_in_I, int *d_in_E, int *d_in_W, int *d_out_D, int *d_out_P);
 
-#endif //GPU_GRAPH_ALGORITHMS_KERNELS_CUH
+#endif //BELLMANFORD_GPU_KERNELS_CUH
