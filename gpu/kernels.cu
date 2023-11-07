407831739970f1b52328aac5ec01ac4edc623996
babsubra1980 <37005639+babsubra1980@users.noreply.github.com>
2023-11-06 18:23:42 -0600

    Update kernels.cu

--- gpu/kernels.cu
+++ gpu/kernels.cu
@@ -1,40 +1,5 @@
 #include "kernels.cuh"
 
-__global__ void relax(int N, int MAX_VAL, int *d_in_V, int *d_in_I, int *d_in_E, int *d_in_W, int *d_out_D, int *d_out_Di) {
-    unsigned int index = threadIdx.x + (blockDim.x * blockIdx.x);
-
-    if (index < N - 1) { // do index < N - 1 because nth element of I array points to the end of E array
-        for (int j = d_in_I[index]; j < d_in_I[index + 1]; j++) {
-            int w = d_in_W[j];
-            int du = d_out_D[index];
-            int dv = d_out_D[d_in_E[j]];
-            int newDist = du + w;
-            // Check if the distance is already set to max then just take the max since,
-            // Cuda implementation gives this when a number is added to already max value of int.
-            // E.g 2147483647 + 5 becomes -2147483644
-            if (du == MAX_VAL){
-                newDist = MAX_VAL;
-            }
-
-            if (newDist < dv) {
-                atomicMin(&d_out_Di[d_in_E[j]],newDist);
-            }
-        }
-    }
-}
-
-
-__global__ void updateDistance(int N, int *d_in_V, int *d_in_I, int *d_in_E, int *d_in_W, int *d_out_D, int *d_out_Di) {
-    unsigned int index = threadIdx.x + (blockDim.x * blockIdx.x);
-    if (index < N) {
-
-        if (d_out_D[index] > d_out_Di[index]) {
-            d_out_D[index] = d_out_Di[index];
-        }
-        d_out_Di[index] = d_out_D[index];
-    }
-}
-
 __global__ void updateDistanceWithGridStride(int N, int *d_in_V, int *d_in_I, int *d_in_E, int *d_in_W, int *d_out_D, int *d_out_Di) {
     unsigned int tid = threadIdx.x + (blockDim.x * blockIdx.x);
     unsigned int stride = blockDim.x * gridDim.x;
@@ -44,29 +9,6 @@ __global__ void updateDistanceWithGridStride(int N, int *d_in_V, int *d_in_I, in
                 d_out_D[index] = d_out_Di[index];
             }
             d_out_Di[index] = d_out_D[index];
-    }
-}
-
-__global__ void updateIndexOfEdges(int N, int *d_in_V, int *d_in_E, int l, int r) {
-    unsigned int index = threadIdx.x + (blockDim.x * blockIdx.x);
-
-    // This does binary search on the V array to find the index of each node in the Edge array (E) and replace the same with index
-    // Based on the iterative binary search from : https://www.geeksforgeeks.org/binary-search/
-    if (index < N) {
-        while (l <= r) {
-            int m = l + (r - l) / 2;
-            // Check if x is present at mid
-            if (d_in_V[m] == d_in_E[index]) {
-                d_in_E[index] = m;
-                break;
-            }
-            // If x greater, ignore left half
-            if (d_in_V[m] < d_in_E[index]) {
-                l = m + 1;
-            } else {        // If x is smaller, ignore right half
-                r = m - 1;
-            }
-        }
     }
 }
 
@@ -99,18 +41,6 @@ __global__ void updateIndexOfEdgesWithGridStide(int N, int *d_in_V, int *d_in_E,
     }
 }
 
-__global__ void initializeArray(const int N, int *p, const int val, bool sourceDifferent, const int source, const int sourceVal){
-    int index = threadIdx.x + blockDim.x * blockIdx.x;
-
-    if (index < N) {
-        p[index] = val;
-        if(sourceDifferent){
-            if(index == source) {
-                p[index] = sourceVal;
-            }
-        }
-    }
-}
 
 __global__ void initializeArrayWithGridStride(const int N, int *p, const int val, bool sourceDifferent, const int source, const int sourceVal){
 
